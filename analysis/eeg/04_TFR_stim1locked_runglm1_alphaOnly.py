c5829c4b804719de2af43fcec7c7accd19c0aa04
Sammi Chekroud <schekrou@fmrib.ox.ac.uk>
2023-06-21 09:56:22 +0100

    add analysis scripts from laptop

---
+++ analysis/eeg/04_TFR_stim1locked_runglm1_alphaOnly.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 10:35:57 2023
+
+@author: sammi
+"""
+import numpy as np
+import scipy as sp
+import pandas as pd
+import mne
+from copy import deepcopy
+import os
+import os.path as op
+import sys
+from matplotlib import pyplot as plt
+from scipy import stats
+import seaborn as sns
+%matplotlib
+
+# sys.path.insert(0, '/ohba/pi/knobre/schekroud/postdoc/student_projects/EffortDifficulty/analysis/tools')
+sys.path.insert(0, '/Users/sammi/Desktop/postdoc/student_projects/EffortDifficulty/analysis/tools')
+# sys.path.insert(0, 'C:/Users/sammi/Desktop/Experiments/postdoc/student_projects/EffortDifficulty/analysis/tools')
+from funcs import getSubjectInfo
+
+# wd = '/ohba/pi/knobre/schekroud/postdoc/student_projects/EffortDifficulty' #workstation wd
+wd = '/Users/sammi/Desktop/postdoc/student_projects/EffortDifficulty'
+# wd = 'C:/Users/sammi/Desktop/Experiments/postdoc/student_projects/EffortDifficulty/'
+os.chdir(wd)
+
+# sys.path.insert(0, '/home/sammirc/Desktop/DPhil/glm')
+import glmtools as glm
+
+
+subs = np.array([10, 11, 12, 13, 14, 15, 16])
+#%%
+glms2run = 1 #1 with no baseline, one where tfr input data is baselined
+for i in subs:
+    for iglm in [1]: #force this to be baselined for now #range(glms2run):
+        print('\nworking on subject ' + str(i) +'\n')
+        sub   = dict(loc = 'laptop', id = i)
+        param = getSubjectInfo(sub)
+        tfr = mne.time_frequency.read_tfrs(param['stim1locked'].replace('stim1locked', 'stim1locked_cleaned_Alpha').replace('-epo.fif', '-tfr.h5')); tfr = tfr[0]
+        
+        #comes with metadata attached            
+        tfr = tfr['fbtrig != 62'] #drop timeout trials
+    
+        # tmpleft  = deepcopy(tfr)['chosenside==0'].average()
+        # tmpright = deepcopy(tfr)['chosenside==1'].average()
+        # tmp_lvsr = tmpleft - tmpright
+    
+        if iglm == 0:
+            addtopath = ''
+            baseline_input = False
+        elif iglm == 1:
+            addtopath = '_baselined'
+            baseline_input = True
+
+        if baseline_input:
+           print(' -- baselining the TFR data -- ')
+           tfr = tfr.apply_baseline((-1, -0.8)) #takes 1 to 0.8s before stim1 onset as baseline, shouldn't interfere with prestim effects here
+    
+        glmdata         = glm.data.TrialGLMData(data = tfr.data, time_dim = 3, sample_rate = 100)
+        nobs = glmdata.num_observations
+        trials = np.ones(glmdata.num_observations) #regressor for just grand mean response
+    
+        # if iglm == 0:
+        #     print('\nSubject %02d has %03d trials\n'%(i, trials.size))
+        correctness = tfr.metadata.PerceptDecCorrect.to_numpy()
+        correct     = tfr.metadata.rewarded.to_numpy()
+        incorrect   = tfr.metadata.unrewarded.to_numpy()
+        correctness = np.where(correctness == 0, -1, correctness)
+        
+        
+        regressors = list()
+
+        #add regressors to the model
+        regressors.append(glm.regressors.CategoricalRegressor(category_list = correct, codes = 1, name = 'correct'))
+        regressors.append(glm.regressors.CategoricalRegressor(category_list = incorrect, codes  = 1, name = 'incorrect'))
+        # regressors.append(glm.regressors.ParametricRegressor(name = 'correctness', values = correctness, preproc = None, num_observations = nobs)) #contrast regressor
+        
+        
+        contrasts = list()
+        contrasts.append(glm.design.Contrast([1, 0], 'correct'))      #0
+        contrasts.append(glm.design.Contrast([0, 1], 'incorrect'))    #1
+        contrasts.append(glm.design.Contrast([1,-1], 'corrvsincorr'))        #2
+        contrasts.append(glm.design.Contrast([1, 1], 'grandmean'))      #3
+        
+        glmdes = glm.design.GLMDesign.initialise(regressors, contrasts)
+        #glmdes.plot_summary()
+            #if iglm == 0:
+            #    glmdes.plot_summary()
+    
+        times = tfr.times
+        freqs = tfr.freqs
+        info = tfr.info
+
+        del(tfr)
+    
+    
+        print('\n - - - - -  running glm - - - - - \n')
+        model = glm.fit.OLSModel( glmdes, glmdata)
+
+        del(glmdata) #clear from RAM as not used from now on really
+#        contrastnames = np.stack([np.arange(len(contrasts)), glmdes.contrast_names], axis=1)
+
+        # loop over betas first and save them
+        for iname in range(len(model.regressor_names)): #loop over regressors
+            if model.regressor_names[iname] in ['grandmean', 'correctness']:
+                nave = len(trials)
+            elif model.regressor_names[iname] == 'correct':
+                nave = correct.sum()
+            elif model.regressor_names[iname] == 'incorrect':
+                nave = incorrect.sum()
+        
+            ibeta_name = model.regressor_names[iname] #regressor name to save
+            
+            tfr_beta = mne.time_frequency.AverageTFR(info = info, times = times, freqs = freqs, nave = nave,
+                                                     data = model.betas[iname])
+            tfr_beta.save(fname = op.join(wd, 'glms', 'stim1locked', 'glm1', 
+                                          param['subid'] + '_stim1locked_tfr_AlphaOnly_' + ibeta_name + '_beta-tfr.h5'), overwrite = True)
+            del(tfr_beta)
+
+        #loop over contrasts + tstats now
+        for iname in range(len(model.contrast_names)):
+            name = model.contrast_names[iname].replace(' ','') #remove whitespace in the contrast name
+
+            if iname in [0]:
+                nave = correct.sum()
+            elif iname in [1]:
+                nave = incorrect.sum()
+            else:
+                nave = len(trials)
+
+            #save betas, contrasts and tstats
+            # betas[count,:,:] = model.betas
+            # copes[count,:,:] = model.copes
+            # tstats[count,:,:] = model.get_tstats()
+            
+
+            tfr_cope = mne.time_frequency.AverageTFR(info = info, times = times, freqs = freqs, nave = nave,
+                                                      data = model.copes[iname])
+            
+            tfr_cope.save(fname = op.join(wd, 'glms', 'stim1locked', 'glm1', 
+                                          param['subid'] + '_stim1locked_tfr_AlphaOnly_' + name + '_cope-tfr.h5'), overwrite = True)
+            del(tfr_cope)
+
+            tfr_tstat = mne.time_frequency.AverageTFR(info = info, times = times, freqs = freqs, nave = nave,
+                                                      data = model.get_tstats()[iname])
+            tfr_tstat.save(fname = op.join(wd, 'glms', 'stim1locked', 'glm1', 
+                                          param['subid'] + '_stim1locked_tfr_AlphaOnly_' + name + '_tstat-tfr.h5'), overwrite = True)
+            del(tfr_tstat)
+        #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        del(glmdes)
+        del(model)
---
+++ analysis/eeg/05_tfr_stim2locked_alphaTimecourse_mainEffects.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 12:55:31 2023
+
+@author: sammi
+"""
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+import mne
+from copy import deepcopy
+import os
+import os.path as op
+import sys
+from matplotlib import pyplot as plt
+from scipy import stats
+import seaborn as sns
+%matplotlib
+
+# sys.path.insert(0, '/ohba/pi/knobre/schekroud/postdoc/student_projects/EffortDifficulty/analysis/tools')
+sys.path.insert(0, '/Users/sammi/Desktop/postdoc/student_projects/EffortDifficulty/analysis/tools')
+# sys.path.insert(0, 'C:/Users/sammi/Desktop/Experiments/postdoc/student_projects/EffortDifficulty/analysis/tools')
+from funcs import getSubjectInfo
+
+# wd = '/ohba/pi/knobre/schekroud/postdoc/student_projects/EffortDifficulty' #workstation wd
+wd = '/Users/sammi/Desktop/postdoc/student_projects/EffortDifficulty'
+# wd = 'C:/Users/sammi/Desktop/Experiments/postdoc/student_projects/EffortDifficulty/'
+os.chdir(wd)
+
+# sys.path.insert(0, '/home/sammirc/Desktop/DPhil/glm')
+import glmtools as glm
+
+
+subs = np.array([10, 11, 12, 13, 14, 15, 16])
+
+def gauss_smooth(array, sigma = 2):
+    return sp.ndimage.gaussian_filter1d(array, sigma=sigma, axis = 1) #smooths across time, given 2d array of trials x time
+
+
+#%%
+
+gmean     = np.empty(shape = [subs.size, 400])
+correct   = np.empty(shape = [subs.size, 400])
+incorrect = np.empty(shape = [subs.size, 400])
+
+d2   = np.empty(shape = [subs.size, 400])
+d4   = np.empty(shape = [subs.size, 400])
+d8   = np.empty(shape = [subs.size, 400])
+d12  = np.empty(shape = [subs.size, 400])
+
+count = -1 #loop over subjects
+for i in subs:
+    count +=1 #add 1 to be able t add data
+    print('\nworking on subject ' + str(i) +'\n')
+    sub   = dict(loc = 'laptop', id = i)
+    param = getSubjectInfo(sub)
+    tfr = mne.time_frequency.read_tfrs(param['stim2locked'].replace('stim2locked', 'stim2locked_cleaned_Alpha').replace('-epo.fif', '-tfr.h5')); tfr = tfr[0]
+    # tfr = mne.time_frequency.read_tfrs(param['stim2locked'].replace('stim2locked', 'stim2locked_cleaned_Alpha').replace('-epo.fif', '-tfr.h5')); tfr = tfr[0]
+    
+    #comes with metadata attached            
+    tfr = tfr['fbtrig != 62'] #drop timeout trials
+    #reduce down to single timecourse of alpha in posterior channels
+    posteriorchannels = ['PO7', 'PO3', 'O1', 'PO8', 'PO4', 'O2',  'Oz', 'POz']
+    
+    #set baseline params to test stuff
+    tmin, tmax = None, None
+    # tmin, tmax = -0.8, -0.5 #baseline period for stim1locked
+    
+    tfrdat = tfr.copy().apply_baseline(baseline = (tmin, tmax)).pick_channels(posteriorchannels).data.copy()
+    tfrdat = np.mean(tfrdat, axis = 2) #average across the frequency band, results in trials x channels x time
+    tfrdat = np.mean(tfrdat, axis = 1) #average across channels now, returns trials x time
+    tfrdat = gauss_smooth(tfrdat, sigma = 2)
+    
+    times = tfr.times
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.plot(times, np.mean(tfrdat, axis=0))
+    # fig.suptitle('grandmean alpha')
+    
+    corr = tfr.metadata.rewarded.to_numpy()
+    incorr = tfr.metadata.unrewarded.to_numpy()
+    difficulty = tfr.metadata.difficultyOri.to_numpy()
+    
+    diff2 = np.where(difficulty  == 2)[0]
+    diff4 = np.where(difficulty  == 4)[0]
+    diff8 = np.where(difficulty  == 8)[0]
+    diff12 = np.where(difficulty == 12)[0]
+
+    corrdat = tfrdat[corr,:]
+    incorrdat = tfrdat[incorr,:]
+    
+    diff2dat  = tfrdat[diff2,:]
+    diff4dat  = tfrdat[diff4,:]
+    diff8dat  = tfrdat[diff8,:]
+    diff12dat = tfrdat[diff12,:]
+    
+    gmean[count]        = tfrdat.copy().mean(axis=0)
+    correct[count]      = corrdat.mean(axis=0)
+    incorrect[count]    = incorrdat.mean(axis=0)
+    
+    d2[count]   = diff2dat.mean(axis=0)
+    d4[count]   = diff4dat.mean(axis=0)
+    d8[count]   = diff8dat.mean(axis=0)
+    d12[count]  = diff12dat.mean(axis=0)
+#%%
+
+#plot some things here
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.axvspan(xmin=-0.0001, xmax = 0, color = '#000000', alpha = 0.9)
+ax.plot(times, gmean.mean(axis=0), label = 'grandmean', color = '#3182bd')
+ax.fill_between(times,
+                y1 = np.subtract(gmean.mean(axis=0), sp.stats.sem(gmean, axis =0, ddof = 0)),
+                y2 = np.add(gmean.mean(axis=0), sp.stats.sem(gmean, axis =0, ddof = 0)), color = '#3182bd', alpha = 0.3)
+# ax.axvline(x = 0, ymin = gmean.mean(0).min(), ymax=gmean.mean(0).max(), ls = 'dashed', lw = 1, color = '#000000')
+fig.legend()
+
+
+#plot by subsequent correctness
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.axvspan(xmin=-0.0001, xmax = 0, color = '#000000', alpha = 0.9)
+
+ax.plot(times, correct.mean(axis=0), label = 'correct', color = '#31a354')
+ax.fill_between(times,
+                y1 = np.subtract(correct.mean(axis=0), sp.stats.sem(correct, axis =0, ddof = 0)),
+                y2 = np.add(correct.mean(axis=0), sp.stats.sem(correct, axis =0, ddof = 0)), color = '#31a354', alpha = 0.3)
+ax.plot(times, incorrect.mean(axis=0), label = 'incorrect', color = '#f03b20')
+ax.fill_between(times,
+                y1 = np.subtract(incorrect.mean(axis=0), sp.stats.sem(incorrect, axis =0, ddof = 0)),
+                y2 = np.add(incorrect.mean(axis=0), sp.stats.sem(incorrect, axis =0, ddof = 0)), color = '#f03b20', alpha = 0.3)
+fig.legend()
+
+
+#plot by difficulty
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.axvspan(xmin=-0.0001, xmax = 0, color = '#000000', alpha = 0.9)
+
+ax.plot(times, d2.mean(axis=0), label = 'difficulty 2', color = '#d7191c')
+ax.fill_between(times,
+                y1 = np.subtract(d2.mean(axis=0), sp.stats.sem(d2, axis =0, ddof = 0)),
+                y2 = np.add(d2.mean(axis=0), sp.stats.sem(d2, axis =0, ddof = 0)), color = '#d7191c', alpha = 0.2)
+ax.plot(times, d4.mean(axis=0), label = 'difficulty 4', color = '#fdae61')
+ax.fill_between(times,
+                y1 = np.subtract(d4.mean(axis=0), sp.stats.sem(d4, axis =0, ddof = 0)),
+                y2 = np.add(d4.mean(axis=0), sp.stats.sem(d4, axis =0, ddof = 0)), color = '#fdae61', alpha = 0.2)
+ax.plot(times, d8.mean(axis=0), label = 'difficulty 8', color = '#a6d96a')
+ax.fill_between(times,
+                y1 = np.subtract(d8.mean(axis=0), sp.stats.sem(d8, axis =0, ddof = 0)),
+                y2 = np.add(d8.mean(axis=0), sp.stats.sem(d8, axis =0, ddof = 0)), color = '#a6d96a', alpha = 0.2)
+ax.plot(times, d12.mean(axis=0), label = 'difficulty 12', color = '#1a9641')
+ax.fill_between(times,
+                y1 = np.subtract(d12.mean(axis=0), sp.stats.sem(d12, axis =0, ddof = 0)),
+                y2 = np.add(d12.mean(axis=0), sp.stats.sem(d12, axis =0, ddof = 0)), color = '#1a9641', alpha = 0.2)
+fig.legend()
